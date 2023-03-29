2f2243feae388d5a85009dfbec0b587275c8edd3
usama2333 <usama.wizz@gmail.com>
2023-03-29 14:53:21 +0500

    Navbar section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  // console.log(process.env.REACT_APP_API_KEY);
-  // console.log(process.env.REACT_APP_OTHER_APP);
+  ;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar/>
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,38 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider , createTheme } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const theme = createTheme({
+  // palette : {
+  //   primary :  {
+  //     main : "#1A3447"
+  //   },
+   
+  // },
+
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xlg : 1500,
+      xl: 1920,
+      custom: 2000, 
+    },
+  },
+ 
+
+})
+
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
